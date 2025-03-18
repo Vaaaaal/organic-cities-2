cf58dd0e7b6fc0563dfa4073c8e8144b5452ca1f
Vaaaaal <cassusval@gmail.com>
2025-03-18 15:47:38 +0100

    resolve small bugs

--- main.js
+++ main.js
@@ -85,31 +85,31 @@ document.addEventListener("DOMContentLoaded", (event) => {
     // Toggle navbar 2025 menu
     $(".navbar-2025_menu").on("click", function() {
         if($(".navbar-2025_menu").find(".navbar-2025_menu").hasClass("is-active")) {
-        $(".navbar-2025_menu").find(".navbar-2025_menu").removeClass("is-active")
-        $(".navbar-2025_menu").find(".navbar-2025_menu-icon_open").css("display", "flex")
-        $(".navbar-2025_menu").find(".navbar-2025_menu-icon_close").css("display", "none")
+            $(".navbar-2025_menu").find(".navbar-2025_menu").removeClass("is-active")
+            $(".navbar-2025_menu").find(".navbar-2025_menu-icon_open").css("display", "flex")
+            $(".navbar-2025_menu").find(".navbar-2025_menu-icon_close").css("display", "none")
 
-        gsap.to(".navbar-2025_tabs-list_wrapper", {
-            opacity: 0,
-            duration: 0.4,
-            ease: "power2.easeOut",
-            onComplete: () => {
-            $(".navbar-2025_tabs-list_wrapper").css("pointer-events", "none")
-            }
-        })
+            gsap.to(".navbar-2025_tabs-list_wrapper", {
+                opacity: 0,
+                duration: 0.4,
+                ease: "power2.easeOut",
+                onComplete: () => {
+                    $(".navbar-2025_tabs-list_wrapper").css("pointer-events", "none")
+                }
+            })
         } else {
-        $(".navbar-2025_menu").find(".navbar-2025_menu").addClass("is-active")
-        $(".navbar-2025_menu").find(".navbar-2025_menu-icon_open").css("display", "none")
-        $(".navbar-2025_menu").find(".navbar-2025_menu-icon_close").css("display", "flex")
+            $(".navbar-2025_menu").find(".navbar-2025_menu").addClass("is-active")
+            $(".navbar-2025_menu").find(".navbar-2025_menu-icon_open").css("display", "none")
+            $(".navbar-2025_menu").find(".navbar-2025_menu-icon_close").css("display", "flex")
 
-        gsap.to(".navbar-2025_tabs-list_wrapper", {
-            opacity: 1,
-            duration: 0.4,
-            ease: "power2.easeIn",
-            onComplete: () => {
-            $(".navbar-2025_tabs-list_wrapper").css("pointer-events", "auto")
-            }
-        })
+            gsap.to(".navbar-2025_tabs-list_wrapper", {
+                opacity: 1,
+                duration: 0.4,
+                ease: "power2.easeIn",
+                onComplete: () => {
+                    $(".navbar-2025_tabs-list_wrapper").css("pointer-events", "auto")
+                }
+            })
         }
     })
 })
