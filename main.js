5bf9e8b2aa943a5ede16eb7a59ae8868f650419a
Vaaaaal <cassusval@gmail.com>
2025-03-28 17:35:00 +0100

    update navbar second

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+    // Add match media to gsap
+    let mm = gsap.matchMedia();
+
     gsap.registerPlugin(Flip, ScrollTrigger, SplitText);
 
     $(".big-btn").on("mouseenter", function() {
@@ -84,11 +87,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     // Toggle navbar 2025 menu
     $(".navbar-2025_menu").on("click", function() {
-        console.log("Click here");
-
         if($(".navbar-2025_menu").hasClass("is-active")) {
-            console.log("Close the menu");
-
             $(".navbar-2025_menu").removeClass("is-active")
             $(".navbar-2025_menu").find(".navbar-2025_menu-icon_open").css("display", "flex")
             $(".navbar-2025_menu").find(".navbar-2025_menu-icon_close").css("display", "none")
@@ -102,8 +101,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 }
             })
         } else {
-            console.log("Open the menu");
-
             $(".navbar-2025_menu").addClass("is-active")
             $(".navbar-2025_menu").find(".navbar-2025_menu-icon_open").css("display", "none")
             $(".navbar-2025_menu").find(".navbar-2025_menu-icon_close").css("display", "flex")
@@ -115,6 +112,41 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 onComplete: () => {
                     $(".navbar-2025_tabs-list_wrapper").css("pointer-events", "auto")
                 }
+            })
+        }
+    })
+
+    gsap.set('.navbar_second',{
+        display: "flex",
+    })
+    
+    // if($("._2025_program-day_timeline-item.is-ball").length) {
+    //     new ScrollTrigger({
+    //         trigger: "._2025_program-day_timeline-item.is-ball",
+    //         start: "top 50vh",
+    //         end: "bottom 50vh",
+    //         pin: true,
+    //         pinReparent: true,
+    //         markers: true,
+    //     })
+    // }
+
+    mm.add("(min-width: 992px)", () => {
+        // Toggle navbar 2025 tabs on scroll top or bottom
+        if ($(".navbar_second").length && !$(".navbar_second").hasClass("is-snapper")) {
+            const navbar2025Tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: "body",
+                    start: "100vh top",
+                    toggleActions: "play none none reverse",
+                }
+            })
+            navbar2025Tl.fromTo(".navbar_second", {
+                yPercent: -110
+            }, {
+                yPercent: 0,
+                duration: 0.3,
+                ease: "power2.easeIn",
             })
         }
     })
